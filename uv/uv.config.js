9e4dc3f7cc9079df842cb25bc3d8650e105100ff
Wave Demure <144980384+WaveDemure@users.noreply.github.com>
2024-01-21 23:07:19 -0800

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://bare.benrogo.net',
+    bare: 'https://snorlaxscave.site/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
